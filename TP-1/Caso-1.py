d05e4bc5835c5ac094b3e444568c6ee98f4e1144
temPLAY333 <t.bourguet@alumno.um.edu.ar>
2024-06-01 19:57:23 -0300

    uy

--- TP-1/Caso-1.py
+++ TP-1/Caso-1.py
@@ -1,69 +1,65 @@
 from mininet.net import Mininet
-from mininet.node import Node
-from mininet.node import RemoteController, OVSKernelSwitch
-from mininet.link import TCLink
+from mininet.node import Node, OVSKernelSwitch
 from mininet.cli import CLI
 
-class CustomRouter(Node):
+# Alumno: Tomás Bourguet
+# Legajo: 61235
+# Caso: 1
+
+class Mi_router_linux(Node):
     def config(self, **params):
-        super(CustomRouter, self).config(**params)
+        super(Mi_router_linux, self).config(**params)
+        self.cmd('sysctl net.ipv4.ip_forward=1')
 
     def terminate(self):
-        super(CustomRouter, self).terminate()
+        super(Mi_router_linux, self).terminate()
 
-def calcular_direcciones():
-    enlaces_wan = {}
-    direcciones_privadas = {}
+def configurar_router(central_router, sucursal_router, num_sucursal):
+    ip_central_wan = f'192.168.100.{6 + (8 * (num_sucursal - 1))}/29'
+    ip_sucursal_wan = f'192.168.100.{1 + (8 * (num_sucursal - 1))}/29'
+
+    central_router.cmd(f'ifconfig {central_router.name}-eth{num_sucursal - 1} {ip_central_wan}')
+    central_router.cmd(f'ip route add 10.0.{num_sucursal}.0/24 via {ip_sucursal_wan.split("/")[0]}')
+
+    sucursal_router.cmd(f'ifconfig {sucursal_router.name}-eth1 {ip_sucursal_wan}')
+    sucursal_router.cmd(f'ip route add 10.0.0.0/8 via {ip_central_wan.split("/")[0]}')
+    sucursal_router.cmd(f'ip route add 192.168.100.0/24 via {ip_central_wan.split("/")[0]}')
+
+def configurar_host(host, num_sucursal):
+    host.cmd(f'ip route add default via 10.0.{num_sucursal}.1')
+
+def crear_red():
+    net = Mininet(switch=OVSKernelSwitch, build=False)
+
+    central_router = net.addHost('rc', cls=Mi_router_linux, ip='192.168.100.6/29')
 
     for num_sucursal in range(1, 7):
-        enlaces_wan[f"suc{num_sucursal}"] = f"192.168.100.{(num_sucursal - 1) * 8}/29"
-        direcciones_privadas[f"suc{num_sucursal}"] = f"10.0.{num_sucursal}.0/24"
+        sw_wan = net.addSwitch(f'swWAN{num_sucursal}', cls=OVSKernelSwitch, failMode='standalone')
+        net.addLink(central_router, sw_wan)
 
-    return enlaces_wan, direcciones_privadas
+        sucursal_router = net.addHost(f'rSuc{num_sucursal}', cls=Mi_router_linux, ip=f'10.0.{num_sucursal}.1/24')
+        sw_lan = net.addSwitch(f'swLAN{num_sucursal}', cls=OVSKernelSwitch, failMode='standalone')
+        net.addLink(sucursal_router, sw_lan)
+        net.addLink(sucursal_router, sw_wan)
 
-def crear_topologia(enlaces_wan, direcciones_privadas):
-    net = Mininet(switch=OVSKernelSwitch, link=TCLink)
-
-    net.addController('c', controller=RemoteController, ip='127.0.0.1', port=6653)
-
-    r_central = net.addHost('rCent')
-
-    for num_sucursal in range(1, 7):
-        sucursal = f"suc{num_sucursal}"
-        dir_wan = enlaces_wan[sucursal]
-
-        ip_router_wan = f"192.168.100.{(num_sucursal - 1) * 8 + 1}/29"
-        ip_central_wan = f"192.168.100.{(num_sucursal - 1) * 8 + 2}/29"
-        ip_router_lan = f"10.0.{num_sucursal}.1/24"
-        ip_puesto1_lan = f"10.0.{num_sucursal}.2/24"
-        ip_puesto2_lan = f"10.0.{num_sucursal}.3/24"
-
-        r_sucursal = net.addHost(f'router{sucursal}', cls=CustomRouter, ip=ip_router_lan)
-
-        p1 = net.addHost(f'p1{sucursal}', ip=ip_puesto1_lan)
-        p2 = net.addHost(f'p2{sucursal}', ip=ip_puesto2_lan)
-
-        sw_wan = net.addSwitch(f'swWAN{sucursal}')
-        sw_lan = net.addSwitch(f'swLAN{sucursal}')
-
-        net.addLink(r_central, sw_wan)
-        r_central.cmd(f'ifconfig {r_central.name}-{num_sucursal - 1} {ip_central_wan}')
-
-        net.addLink(r_sucursal, sw_wan)
-        r_sucursal.cmd(f'ifconfig {r_sucursal.name} {ip_router_wan}')
-
-        net.addLink(r_sucursal, sw_lan)
-        net.addLink(p1, sw_lan)
-        net.addLink(p2, sw_lan)
-
-        r_sucursal.cmd(f'ip route add 192.168.100.0/24 via {ip_central_wan}')
-        r_central.cmd(f'ip route add {direcciones_privadas[sucursal]} via {ip_router_wan}')
+        for host_num in range(1, 3):
+            host = net.addHost(f'h{host_num}Suc{num_sucursal}', ip=f'10.0.{num_sucursal}.{host_num + 1}/24')
+            net.addLink(host, sw_lan)
 
     net.build()
-    net.start()
+
+    for switch in net.switches:
+        switch.start([])
+
+    for num_sucursal in range(1, 7):
+        sucursal_router = net.get(f'rSuc{num_sucursal}')
+        configurar_router(central_router, sucursal_router, num_sucursal)
+        for host_num in range(1, 3):
+            host = net.get(f'h{host_num}Suc{num_sucursal}')
+            configurar_host(host, num_sucursal)
+
     CLI(net)
     net.stop()
 
 if __name__ == '__main__':
-    enlaces_wan, direcciones_privadas = calcular_direcciones()
-    crear_topologia(enlaces_wan, direcciones_privadas)
+    crear_red()
